4e47e9749d180ff2060a19ba4692a4c6018b03de
Najma Jama <Najma55.nj@gmail.com>
2023-09-30 11:30:19 -0500

    create html structure

--- src/components/ViewMovie/ViewMovie.jsx
+++ src/components/ViewMovie/ViewMovie.jsx
@@ -1,9 +1,24 @@
 import React from "react";
-import { useParams} from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
-export default function ViewMovie(){
-    const params = useParams();
-    return <div>HI {params.id}</div>
-   
-    
+export default function ViewMovie() {
+  const params = useParams();
+  return (
+    <div
+      className="movie-page"
+      style={{
+        backgroundImage: `url('/images/AVATAR.png')`,
+      }}
+    >
+      <div className="left">
+        <h1>DeadPool</h1>
+        <p>
+          In dolor fugiat ex minim aute dolor eiusmod duis mollit. In voluptate
+          ut fugiat ut nostrud. Sunt aliqua esse irure proident. Consectetur
+          nisi exercitation sunt ipsum.
+        </p>
+        <button>WATCH NOW</button>
+      </div>
+    </div>
+  );
 }
