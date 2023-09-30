51295a72faad3478f0f9cefbf8abd3912af279f8
Najma Jama <Najma55.nj@gmail.com>
2023-09-30 12:37:24 -0500

    render the movie details on the dom

--- src/components/ViewMovie/ViewMovie.jsx
+++ src/components/ViewMovie/ViewMovie.jsx
@@ -1,32 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function ViewMovie() {
   const params = useParams();
+  // Get the fetched movie from the store---2
+  const oneMovie = useSelector((store) => store.oneMovie);
+  console.log(oneMovie);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    // Get one movie from the server---- 1
+    dispatch({ type: "FETCH_ONEMOVIE", payload: params.id });
+  }, []);
+
+  const [movie, setMovie] = useState(null);
+
+  // format data----3
+  useEffect(() => {
+    if (oneMovie) {
+      setMovie({
+        title: oneMovie[0].title,
+        poster: oneMovie[0].poster,
+        description: oneMovie[0].description,
+        // Get genre names from all records
+        genres: oneMovie.map((one) => one.name),
+      });
+    }
+  }, [oneMovie]);
+
   return (
     <div
       className="movie-page"
       style={{
-        backgroundImage: `url('/images/AVATAR.png')`,
+        backgroundImage: `url(${movie?.poster})`,
       }}
-    >
-      <div className="left">
-        <h1>DeadPool</h1>
-        <p>
-          In dolor fugiat ex minim aute dolor eiusmod duis mollit. In voluptate
-          ut fugiat ut nostrud. Sunt aliqua esse irure proident. Consectetur
-          nisi exercitation sunt ipsum.
-        </p>
-        <div className="genre">
-          <span>Genre</span>
-          <p className="list">
-            <span>Thriller,</span>
-            <span>Thriller,</span>
-            <span>Thriller</span>
+    ><div className="overlay"></div>
+      {movie ? (
+        <div className="left">
+          <h1>{movie?.title}</h1>
+          <p>
+            {movie?.description}
           </p>
+          <div className="genre">
+            <span>Genre</span>
+            <p className="list">
+              {movie?.genres.map((oneGenre)=>(
+                <span>{oneGenre}</span>
+              ))}
+            </p>
+          </div>
+          <button>WATCH NOW</button>
         </div>
-        <button>WATCH NOW</button>
-      </div>
+      ) : null}
     </div>
   );
 }
